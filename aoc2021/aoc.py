517e720ea44f809ef094302653e610578b2acc39
Abzol <hakurou46@gmail.com>
2021-12-01 22:37:52 +0100

    lets start 2021

---
+++ aoc2021/aoc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import importlib
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 3):
+        with open(f'{sys.argv[1]}/input') as f:
+            answer = importlib.import_module(sys.argv[1])
+            if (sys.argv[2] == "1"):
+                print(answer.ver_one(f))
+            elif (sys.argv[2] == "2"):
+                print(answer.ver_two(f))
+            else:
+                print("No such answer")
+    else:
+        print("Missing arguments")
