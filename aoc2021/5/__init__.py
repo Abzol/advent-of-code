89e503774c107bc8e5a43fb3969acfc003aa9fc2
Abzol <hakurou46@gmail.com>
2021-12-06 00:29:12 +0100

    day 5 part 1

---
+++ aoc2021/5/__init__.py
@@ -0,0 +1,43 @@
+import sys
+import re
+from math import copysign
+
+class Ventline():
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    def is_aligned(self):
+        return (self.x1 == self.x2 or self.y1 == self.y2)
+    def get_coverage(self):
+        if self.is_aligned():
+            l = abs(self.x2 - self.x1) + abs(self.y2 - self.y1)
+            x = self.x1
+            y = self.y1
+            for i in range(l+1):
+                yield f'{int(x)},{int(y)}'
+                if self.x1 != self.x2:
+                    x = x + copysign(1, (self.x2-self.x1))
+                if self.y1 != self.y2:
+                    y = y + copysign(1, (self.y2-self.y1))
+        else:
+            return None
+
+def ver_one(input):
+    lines = input.readlines()
+    ventpattern = {}
+    for line in lines:
+        data = [int(x) for x in re.split('->|,', line)]
+        vents = Ventline(data[0], data[1], data[2], data[3])
+        if vents.is_aligned():
+            for x in vents.get_coverage():
+                if x not in ventpattern:
+                    ventpattern[x] = 1
+                else:
+                    ventpattern[x] = ventpattern[x] + 1
+    t = 0
+    for k in ventpattern:
+        if ventpattern[k] > 1:
+            t+=1
+    return t
