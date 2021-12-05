10ec877bc094def9e0006ac942aa1b963eb49cbd
Abzol <hakurou46@gmail.com>
2021-12-06 00:45:34 +0100

    oh that was... easy

--- aoc2021/5/__init__.py
+++ aoc2021/5/__init__.py
@@ -11,18 +11,15 @@ class Ventline():
     def is_aligned(self):
         return (self.x1 == self.x2 or self.y1 == self.y2)
     def get_coverage(self):
-        if self.is_aligned():
-            l = abs(self.x2 - self.x1) + abs(self.y2 - self.y1)
-            x = self.x1
-            y = self.y1
-            for i in range(l+1):
-                yield f'{int(x)},{int(y)}'
-                if self.x1 != self.x2:
-                    x = x + copysign(1, (self.x2-self.x1))
-                if self.y1 != self.y2:
-                    y = y + copysign(1, (self.y2-self.y1))
-        else:
-            return None
+        l = abs(self.x2 - self.x1) or abs(self.y2 - self.y1)
+        x = self.x1
+        y = self.y1
+        for i in range(l+1):
+            yield f'{int(x)},{int(y)}'
+            if self.x1 != self.x2:
+                x = x + copysign(1, (self.x2-self.x1))
+            if self.y1 != self.y2:
+                y = y + copysign(1, (self.y2-self.y1))
 
 def ver_one(input):
     lines = input.readlines()
@@ -36,6 +33,23 @@ def ver_one(input):
                     ventpattern[x] = 1
                 else:
                     ventpattern[x] = ventpattern[x] + 1
+    t = 0
+    for k in ventpattern:
+        if ventpattern[k] > 1:
+            t+=1
+    return t
+
+def ver_two(input):
+    lines = input.readlines()
+    ventpattern = {}
+    for line in lines:
+        data = [int(x) for x in re.split('->|,', line)]
+        vents = Ventline(data[0], data[1], data[2], data[3])
+        for x in vents.get_coverage():
+            if x not in ventpattern:
+                ventpattern[x] = 1
+            else:
+                ventpattern[x] = ventpattern[x] + 1
     t = 0
     for k in ventpattern:
         if ventpattern[k] > 1:
