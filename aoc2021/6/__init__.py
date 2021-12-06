5ebe6d17a3b5f2138099e9df5c3359b327fc3ceb
Abzol <hakurou46@gmail.com>
2021-12-06 06:57:49 +0100

    cool solve

--- aoc2021/6/__init__.py
+++ aoc2021/6/__init__.py
@@ -1,5 +1,6 @@
 import sys
 
+#bad
 def simulate_fish(seed, days):
     fish = seed
     for day in range(days):
@@ -12,10 +13,21 @@ def simulate_fish(seed, days):
                 new_fish.append(8)
     return len(fish)
 
+#fasty as fuck
+def simulate_many_fish(seed, days):
+    fish = [0,0,0,0,0,0,0,0,0]
+    for x in seed:
+        fish[x] += 1
+    for day in range(days):
+        spawn = fish.pop(0)
+        fish.append(spawn)
+        fish[6] += spawn
+    return sum(fish)
+
 def ver_one(input):
     fish = [int(x) for x in input.read().split(',')]
-    return simulate_fish(fish, 80)
+    return simulate_many_fish(fish, 80)
 
 def ver_two(input):
     fish = [int(x) for x in input.read().split(',')]
-    return None #slow as hell
+    return simulate_many_fish(fish, 256)
